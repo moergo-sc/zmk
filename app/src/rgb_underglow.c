13603b56bb85d0d6194be349c0fd4f3ef9d0ff0f
Sami Yildirim <sapphirewhalez@gmail.com>
2024-12-31 16:06:02 +1100

    chore(underglow): Add missing include header

--- app/src/rgb_underglow.c
+++ app/src/rgb_underglow.c
@@ -28,6 +28,7 @@
 
 #include <zmk/activity.h>
 #include <zmk/event_manager.h>
+#include <zmk/events/position_state_changed.h>
 #include <zmk/events/activity_state_changed.h>
 #include <zmk/events/usb_conn_state_changed.h>
 #include <zmk/workqueue.h>
