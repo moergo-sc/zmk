2b389de74ec607f832f3724957d3444b7570cdf5
darknao <darknao@drkn.ninja>
2025-01-04 20:01:15 +0100

    underglow-layer: use behaviors to control RGBs

---
+++ app/include/zmk/events/underglow_color_changed.h
@@ -0,0 +1,15 @@
+/*
+ * Copyright (c) 2024 The ZMK Contributors
+ *
+ * SPDX-License-Identifier: MIT
+ */
+
+#pragma once
+
+#include <zmk/event_manager.h>
+
+struct zmk_underglow_color_changed {
+    uint32_t layers;
+};
+
+ZMK_EVENT_DECLARE(zmk_underglow_color_changed);
